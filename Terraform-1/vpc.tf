5c6ed10ec475b3b47889f2d2140074a3588019b3
kundansingh27feb@gmail.com <kundansingh27feb@gmail.com>
2023-06-11 13:18:12 +0530

    Commit message

---
+++ Terraform-1/vpc.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-west-2" 
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/24"
+  
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "MySubnet"
+  }
+}
